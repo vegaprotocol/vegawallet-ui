1fb6c0b96267efb73cd1c67deef6e34199b6b0b2
Botond <botond@vegaprotocol.io>
2022-11-15 09:05:07 +0000

    fix: jest configs

--- libs/wallet-client/jest.config.ts
+++ libs/wallet-client/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'positions',
+  displayName: 'wallet-client',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,6 +11,5 @@ export default {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/positions',
-  setupFilesAfterEnv: ['./src/setup-tests.ts'],
+  coverageDirectory: '../../coverage/libs/wallet-client',
 }
