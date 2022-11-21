0d9d57b387c8be175f604d3ed21f8e8315140771
Botond <botond@vegaprotocol.io>
2022-11-21 13:26:30 +0000

    fix: feature types

--- libs/wallet-ui/src/types/features.ts
+++ libs/wallet-ui/src/types/features.ts
@@ -1,5 +1,9 @@
-export enum FeatureMap {
-  NETWORK_COMPATIBILITY_WARNING = 'NETWORK_COMPATIBILITY_WARNING',
+export enum FeatureType {
+  NETWORK_COMPATIBILITY_WARNING = 'NETWORK_COMPATIBILITY_WARNING'
 }
 
-export type Features = Record<FeatureMap, boolean>
+export type Features = Record<FeatureType, boolean>
+
+export const FeatureMap: Record<FeatureType, FeatureType> = {
+  [FeatureType.NETWORK_COMPATIBILITY_WARNING]: FeatureType.NETWORK_COMPATIBILITY_WARNING
+}
