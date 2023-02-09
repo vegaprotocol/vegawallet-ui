5a82ebd330e44d588dc3f2708054b9d5f7359a62
Botond <botond@vegaprotocol.io>
2023-02-09 11:31:00 +0000

    fix: clean up test builds

--- libs/wallet-ui/test/index.tsx
+++ libs/wallet-ui/test/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { App as WalletUI } from '../src'
 import { service, client, runtime } from '../src/mocks'
 
-
 const element = document.getElementById('app')
 
 if (element) {
