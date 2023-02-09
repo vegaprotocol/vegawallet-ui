3d3b5c829fd7a9d5c97c1c6fb3691ec180460826
Botond <botond@vegaprotocol.io>
2023-02-09 15:24:21 +0000

    fix: try fixing tests

--- libs/wallet-client/src/index.spec.ts
+++ libs/wallet-client/src/index.spec.ts
@@ -1,37 +1,119 @@
-import fetch from 'node-fetch'
+import nodefetch from 'node-fetch'
 import { WalletClient, Identifier } from './'
 import { MockWalletService } from './mocks'
 
 const PORT = 5199
 const HOSTNAME = `http://localhost:${PORT}`
 
-// @ts-ignore Expected global fetch type doesn't match the lib for some reason
-global.fetch = fetch
-
 const service = new MockWalletService({ port: PORT })
 
-beforeEach(() => {
-  service.start()
-})
+declare global {
+  interface browser {
+    runtime: Runtime
+  }
+  interface chrome {
+    runtime: Runtime
+  }
+}
 
-afterEach(() => {
-  service.stop()
-})
+type Runtime = {
+  sendMessage: () => void
+}
+
+const mockRuntime = {
+  sendMessage: jest.fn(),
+}
+
+// @ts-ignore imported fetch type is different from the one sitting in the types of jest dom
+global.fetch = nodefetch
 
 describe('Wallet Client', () => {
-  it('can connect to a wallet', async () => {
-    const onTokenChange = jest.fn()
-    const client = new WalletClient({ address: HOSTNAME, onTokenChange })
-    const { result } = await client.ConnectWallet()
-    expect(result).toBe('null')
-    expect(onTokenChange).toHaveBeenCalledWith('VWT token')
+  describe('HTTP', () => {
+    beforeEach(() => {
+      mockRuntime.sendMessage.mockReset()
+      service.start()
+    })
+
+    afterEach(() => {
+      service.stop()
+    })
+
+    it('can connect to a wallet', async () => {
+      const onTokenChange = jest.fn()
+      const client = new WalletClient({
+        type: 'http',
+        address: HOSTNAME,
+        onTokenChange,
+      })
+      const result = await client.ConnectWallet()
+      expect(result).toBe('null')
+      expect(onTokenChange).toHaveBeenCalledWith('VWT token')
+    })
+
+    it('returns a supported list of methods', async () => {
+      const onTokenChange = jest.fn()
+      const client = new WalletClient({
+        type: 'http',
+        address: HOSTNAME,
+        onTokenChange,
+      })
+
+      const { registeredMethods } = await client.ListMethods()
+      expect(registeredMethods).toEqual(Object.values(Identifier))
+    })
   })
 
-  it('returns a supported list of methods', async () => {
-    const onTokenChange = jest.fn()
-    const client = new WalletClient({ address: HOSTNAME, onTokenChange })
+  describe('Browser', () => {
+    describe('Chrome', () => {
+      beforeEach(() => {
+        // @ts-ignore ts fails to recognise this
+        global.chrome = {
+          runtime: mockRuntime,
+        }
+      })
 
-    const { registeredMethods } = await client.ListMethods()
-    expect(registeredMethods).toEqual(Object.values(Identifier))
+      afterEach(() => {
+        // @ts-ignore ts fails to recognise this
+        global.chrome = undefined
+      })
+
+      it('can connect to a wallet', async () => {
+        const onTokenChange = jest.fn()
+        const client = new WalletClient({
+          type: 'browser',
+          firefoxId: 'TEST_FIREFOX_ID',
+          chromeId: 'TEST_CHROME_ID',
+        })
+        const { result } = await client.ConnectWallet()
+        expect(result).toBe('null')
+        expect(onTokenChange).toHaveBeenCalledWith('VWT token')
+      })
+    })
+
+    describe('Firefox', () => {
+      beforeEach(() => {
+        // @ts-ignore ts fails to recognise this
+        globalThis.browser = {
+          runtime: mockRuntime,
+        }
+      })
+
+      afterEach(() => {
+        // @ts-ignore ts fails to recognise this
+        globalThis.browser = undefined
+      })
+
+      it('can connect to a wallet', async () => {
+        const onTokenChange = jest.fn()
+        const client = new WalletClient({
+          type: 'browser',
+          firefoxId: 'TEST_FIREFOX_ID',
+          chromeId: 'TEST_CHROME_ID',
+        })
+        const result = await client.ConnectWallet()
+        expect(result).toBe('null')
+        expect(onTokenChange).toHaveBeenCalledWith('VWT token')
+      })
+    })
   })
 })
