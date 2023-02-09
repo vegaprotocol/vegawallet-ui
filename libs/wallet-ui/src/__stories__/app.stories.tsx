7eb381318a72f0acbee2d337d52c82afa11092af
Botond <botond@vegaprotocol.io>
2023-02-09 11:55:10 +0000

    fix: lint

--- libs/wallet-ui/src/__stories__/app.stories.tsx
+++ libs/wallet-ui/src/__stories__/app.stories.tsx
@@ -1,8 +1,11 @@
 import type { Story } from '@storybook/react'
 import type { AppProps } from '../'
 import { App } from '../'
+// eslint-disable-next-line jest/no-mocks-import
 import { service } from '../__mocks__/service'
+// eslint-disable-next-line jest/no-mocks-import
 import { client } from '../__mocks__/client'
+// eslint-disable-next-line jest/no-mocks-import
 import { runtime } from '../__mocks__/runtime'
 
 export default {
