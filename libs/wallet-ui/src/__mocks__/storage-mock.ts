5dcb478b017ae14c40de6147c1b2db1d6c52f96a
Rado <szpiechrados@gmail.com>
2023-03-06 17:19:35 +0100

    chore: property fix

--- libs/wallet-ui/src/__mocks__/storage-mock.ts
+++ libs/wallet-ui/src/__mocks__/storage-mock.ts
@@ -7,8 +7,7 @@ export default function getStorageMock(
     window.localStorage.getItem(`MOCK.${functionName}`) !== null
   ) {
     return JSON.parse(
-      window.localStorage.getItem(`MOCK.service.${functionName}`)?.toString() ||
-        'null'
+      window.localStorage.getItem(`MOCK.${functionName}`)?.toString() || 'null'
     )
   } else {
     return defaultValue || null
