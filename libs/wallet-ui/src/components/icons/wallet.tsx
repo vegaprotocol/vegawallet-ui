0bae96dcfec8dde6e888b672716ee07af5bb6ed3
Dexter <dexter.edwards93@gmail.com>
2023-03-09 10:41:34 +0000

    chore: add icons

---
+++ libs/wallet-ui/src/components/icons/wallet.tsx
@@ -0,0 +1,22 @@
+export const Wallet = () => {
+  return (
+    <svg
+      width="21"
+      height="19"
+      viewBox="0 0 21 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="1.33331" y="1" width="17" height="17" stroke="white" />
+      <rect
+        x="13.3333"
+        y="6"
+        width="7"
+        height="7"
+        fill="black"
+        stroke="white"
+      />
+      <rect x="15.8333" y="8.5" width="2" height="2" fill="white" />
+    </svg>
+  )
+}
