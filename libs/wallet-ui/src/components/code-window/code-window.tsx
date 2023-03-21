9df996a5eadee8885f00b16f08fd721cb8cb7f84
Dexter <dexter.edwards93@gmail.com>
2023-03-21 15:25:05 +0000

    chore: make code window more reusable

--- libs/wallet-ui/src/components/code-window/code-window.tsx
+++ libs/wallet-ui/src/components/code-window/code-window.tsx
@@ -1,9 +1,16 @@
+import type { ReactNode } from 'react'
 import { CopyWithTooltip } from '../copy-with-tooltip'
 
-export const CodeWindow = ({ text }: { text: string }) => {
+export const CodeWindow = ({
+  content,
+  text,
+}: {
+  content: ReactNode
+  text: string
+}) => {
   return (
-    <div className="max-h-40 text-xl flex text-white border-dark-200 border border-2 p-5 rounded-md w-full">
-      <code className="overflow-y-scroll">{text}</code>
+    <div className="max-h-40 text-xl flex border-dark-200 border border-2 p-5 rounded-md w-full">
+      <code className="overflow-y-scroll w-full">{content}</code>
       <CopyWithTooltip text={text} />
     </div>
   )
