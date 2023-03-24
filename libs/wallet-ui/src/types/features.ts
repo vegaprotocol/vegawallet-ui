111724e1c9f4f1f4d14a0efbf8824045a4698e50
Dexter <dexter.edwards93@gmail.com>
2023-03-24 16:37:18 +0000

    feat: add fairground feature flag

--- libs/wallet-ui/src/types/features.ts
+++ libs/wallet-ui/src/types/features.ts
@@ -1,11 +1,13 @@
 export enum FeatureType {
   TELEMETRY_CHECK = 'TELEMETRY_CHECK',
   NETWORK_COMPATIBILITY_WARNING = 'NETWORK_COMPATIBILITY_WARNING',
+  FAIRGROUND_MODE = 'FAIRGROUND_MODE',
 }
 
 export type Features = Record<FeatureType, boolean>
 
 export const FeatureMap: Record<FeatureType, FeatureType> = {
+  [FeatureType.FAIRGROUND_MODE]: FeatureType.FAIRGROUND_MODE,
   [FeatureType.TELEMETRY_CHECK]: FeatureType.TELEMETRY_CHECK,
   [FeatureType.NETWORK_COMPATIBILITY_WARNING]:
     FeatureType.NETWORK_COMPATIBILITY_WARNING,
