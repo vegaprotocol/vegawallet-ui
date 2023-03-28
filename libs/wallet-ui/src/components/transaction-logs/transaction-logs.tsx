d6ba0f92d80ac98fedf4ed9d93d2f2f5b7903e8a
Dexter <dexter.edwards93@gmail.com>
2023-03-28 12:26:02 +0100

    test: fix e2e tests

--- libs/wallet-ui/src/components/transaction-logs/transaction-logs.tsx
+++ libs/wallet-ui/src/components/transaction-logs/transaction-logs.tsx
@@ -11,7 +11,6 @@ type Props = {
 export const TransactionLogs = ({ logs, isVisible, className }: Props) => {
   return (
     <CodeWindow
-      data-testid="transaction-logs"
       text={logs.join('\n')}
       content={logs.map((entry, i) => (
         <p
