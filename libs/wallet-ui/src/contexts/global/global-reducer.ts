abe2c6cbc5cad9f00ccfb4f8720ad5bb04793e01
Dexter <dexter.edwards93@gmail.com>
2023-03-31 14:13:15 +0100

    fix: remove test transaction

--- libs/wallet-ui/src/contexts/global/global-reducer.ts
+++ libs/wallet-ui/src/contexts/global/global-reducer.ts
@@ -15,7 +15,6 @@ import type {
   Wallet,
 } from './global-context'
 import { AppStatus, DrawerPanel, ServiceState } from './global-context'
-import { TransactionKeys, TransactionStatus } from '@vegaprotocol/wallet-types'
 
 export const initialGlobalState: GlobalState = {
   status: AppStatus.Pending,
@@ -26,48 +25,7 @@ export const initialGlobalState: GlobalState = {
   // Wallet
   wallet: null,
   wallets: {},
-  transactions: {
-    1: {
-      id: '1',
-      type: TransactionKeys.WITHDRAW_SUBMISSION,
-      hostname: 'vega.xyz',
-      wallet: 'Wallet 1',
-      publicKey:
-        'c1d9b39e5148b14d694020572cb591a8af971b9c5a4a185f3afa47bd9247c0da',
-      payload: {
-        nonce: '18290134391243719184',
-        blockHeight: '39347',
-        orderSubmission: {
-          marketId:
-            '10c7d40afd910eeac0c2cad186d79cb194090d5d5f13bd31e14c49fd1bded7e2',
-          price: '0',
-          size: '62',
-          side: 'SIDE_BUY',
-          timeInForce: 'TIME_IN_FORCE_GTT',
-          expiresAt: '1679416294197900655',
-          type: 'TYPE_LIMIT',
-          reference: 'traderbot',
-          peggedOrder: {
-            reference: 'PEGGED_REFERENCE_BEST_BID',
-            offset: '9',
-          },
-        },
-      },
-      status: TransactionStatus.SUCCESS,
-      receivedAt: new Date(),
-      logs: [
-        { type: 'Info', message: 'Withdrawal submitted to the network' },
-        { type: 'Warning', message: 'Withdrawal submitted to the network' },
-        { type: 'Error', message: 'Withdrawal submitted to the network' },
-        { type: 'Success', message: 'Withdrawal submitted to the network' },
-      ],
-      txHash:
-        '9BD4215DCDA4DC87F05305BBEF2544261771E9BC2986F78D96B86FD03ACAD4C0',
-      blockHeight: 123456,
-      signature:
-        'signaturesignaturesignaturesignaturesignaturesignaturesignaturesignature',
-    },
-  },
+  transactions: {},
 
   // Network
   currentNetwork: null,
