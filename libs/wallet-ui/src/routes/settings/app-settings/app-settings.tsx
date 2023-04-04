9d422eb1bc04d99ae0ca2459613fedeac3c2b632
Dexter <dexter.edwards93@gmail.com>
2023-04-04 10:15:29 +0100

    chore: helper text

--- libs/wallet-ui/src/routes/settings/app-settings/app-settings.tsx
+++ libs/wallet-ui/src/routes/settings/app-settings/app-settings.tsx
@@ -9,16 +9,17 @@ import {
 import { Page } from '../../../components/page'
 import { LogLevels } from '../../../config/log-levels'
 
+const HelperText = ({ text }: { text: string }) => (
+  <div className="mt-1 text-sm text-neutral-light">{text}</div>
+)
+
 export const AppSettings = () => {
   return (
     <Page name="App settings" back={true}>
       <form>
         <FormGroup label="Wallet directory" labelFor="wallet-directory">
           <Input type="text" id="wallet-directory" />
-          <p>
-            This specifies where the app writes and reads it data. Warning!
-            Changing this will remove existing wallets.
-          </p>
+          <HelperText text="This specifies where the app writes and reads it data. Warning! Changing this will remove existing wallets." />
         </FormGroup>
         <FormGroup label="Log level" labelFor="log-level">
           <Select id="log-level">
@@ -28,18 +29,18 @@ export const AppSettings = () => {
               </option>
             ))}
           </Select>
-          <p>Logs can be found in your Vega home directory.</p>
+          <HelperText text="Logs can be found in your Vega home directory." />
         </FormGroup>
         <FormGroup label="Report bugs and crashes" labelFor="telemetry">
           <RadioGroup value={undefined}>
             <Radio id="associate-radio-contract" label={'Yes'} value={'true'} />
             <Radio id="associate-radio-wallet" label={'No'} value={'false'} />
           </RadioGroup>
-          <p className="mt-1 text-sm text-neutral--light">
-            Selecting yes will help developers improve the software.
-          </p>
+          <HelperText text="Selecting yes will help developers improve the software." />
         </FormGroup>
-        <Button variant="primary">Update and restart</Button>
+        <Button className="mt-4" variant="primary">
+          Update and restart
+        </Button>
       </form>
     </Page>
   )
