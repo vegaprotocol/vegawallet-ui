6a799ee4ee12c08aa382bcab6587336ee8df0bc2
Rado <szpiechrados@gmail.com>
2023-04-05 18:50:04 +0200

    chore: wallet page object added and more tests

--- apps/wallet-mock/src/test/wallets.test.ts
+++ apps/wallet-mock/src/test/wallets.test.ts
@@ -1,10 +1,13 @@
 import { test, expect } from '@playwright/test'
 import { mock } from '../support/mock'
 import adminMock from '../types/admin-mock'
+import walletsPage from '../page/wallets.page'
 
 test.describe('Wallets view', () => {
+  let wallets: ReturnType<typeof walletsPage>
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
+    wallets = walletsPage(page)
   })
 
   test('should see wallets header', async ({ page }) => {
@@ -13,32 +16,33 @@ test.describe('Wallets view', () => {
     ).toHaveText('Wallets')
   })
 
-  test('should see wallets list', async ({ page }) => {
-    await expect(page.getByTestId('wallet-list')).toBeVisible()
+  test('should see wallets list', async () => {
+    await expect(wallets.walletList).toBeVisible()
   })
 
-  test('should see create wallet button', async ({ page }) => {
-    await expect(page.getByTestId('create-new-wallet')).toBeVisible()
+  test('should see create wallet button', async () => {
+    await expect(wallets.createWalletButton).toBeVisible()
   })
 
-  test('should see import wallet button', async ({ page }) => {
-    await expect(page.getByTestId('import-wallet')).toBeVisible()
+  test('should see import wallet button', async () => {
+    await expect(wallets.importWalletButton).toBeVisible()
   })
 })
 
 test.describe('Wallets list', () => {
+  let wallets: ReturnType<typeof walletsPage>
+
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
+    wallets = walletsPage(page)
   })
 
   test('should see empty list', async ({ page }) => {
     await mock(page, adminMock.ListWallets, { wallets: [] })
     await page.goto('/')
 
-    await expect(page.getByTestId('wallet-list')).toBeVisible()
-    await expect(
-      page.getByTestId('wallet-list').getByRole('button')
-    ).toBeHidden()
+    await expect(wallets.walletList).toBeVisible()
+    await expect(wallets.walletList.getByRole('button')).toBeHidden()
   })
 
   test('should see list with few wallets', async ({ page }) => {
@@ -47,10 +51,8 @@ test.describe('Wallets list', () => {
     })
     await page.goto('/')
 
-    await expect(page.getByTestId('wallet-list')).toBeVisible()
-    expect(
-      await page.getByTestId('wallet-list').getByRole('button').all()
-    ).toHaveLength(3)
+    await expect(wallets.walletList).toBeVisible()
+    expect(await wallets.walletList.getByRole('button').all()).toHaveLength(3)
   })
 
   test('should see list with many wallets', async ({ page }) => {
@@ -59,26 +61,61 @@ test.describe('Wallets list', () => {
     })
     await page.goto('/')
 
-    await expect(page.getByTestId('wallet-list')).toBeVisible()
-    expect(
-      await page.getByTestId('wallet-list').getByRole('button').all()
-    ).toHaveLength(100)
+    await expect(wallets.walletList).toBeVisible()
+    expect(await wallets.walletList.getByRole('button').all()).toHaveLength(100)
     await expect(page.getByTestId('wallet-wallet-99')).toBeVisible()
   })
 })
 
 test.describe('Wallets actions', () => {
+  let wallets: ReturnType<typeof walletsPage>
   test.beforeEach(async ({ page }) => {
+    wallets = walletsPage(page)
     await page.goto('/')
   })
 
   test('should open create wallet form', async ({ page }) => {
-    await page.getByTestId('create-new-wallet').click()
+    await wallets.createWalletButton.click()
     await expect(page.getByTestId('create-wallet-form')).toBeVisible()
   })
 
   test('should open import wallet form', async ({ page }) => {
-    await page.getByTestId('import-wallet').click()
+    await wallets.importWalletButton.click()
     await expect(page.getByTestId('import-wallet-form')).toBeVisible()
+  })
+})
+
+test.describe('Wallet item view', () => {
+  let wallets: ReturnType<typeof walletsPage>
+  test.beforeEach(async ({ page }) => {
+    wallets = walletsPage(page)
+    await page.goto('/')
+  })
+
+  test('should see wallet name', async () => {
+    await expect(wallets.wallet('wallet-1')).toBeVisible()
+    await expect(
+      wallets.wallet('wallet-1').getByTestId('wallet-name')
+    ).toHaveText('wallet-1')
+  })
+
+  test('should see wallet locked icon', async () => {
+    await expect(wallets.wallet('wallet-1')).toBeVisible()
+    await expect(
+      wallets.wallet('wallet-1').getByTestId('lock-icon')
+    ).toBeVisible()
+  })
+})
+
+test.describe('Wallet item actions', () => {
+  let wallets: ReturnType<typeof walletsPage>
+  test.beforeEach(async ({ page }) => {
+    wallets = walletsPage(page)
+    await page.goto('/')
+  })
+
+  test('should open passphrase form', async ({ page }) => {
+    await wallets.wallet('wallet-1').click()
+    await expect(page.getByTestId('passphrase-form')).toBeVisible()
   })
 })
