e8ff56ef29ee6f1ab3731b3623550802c0b78002
Rado <szpiechrados@gmail.com>
2023-04-20 12:41:51 +0200

    chore: fix failing wallet list and remove connection tests

--- apps/wallet-mock/src/test/wallets.test.ts
+++ apps/wallet-mock/src/test/wallets.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
-// import { mock } from '../support/mock'
-// import adminMock from '../types/admin-mock'
+import { mock } from '../support/mock'
+import adminMock from '../types/admin-mock'
 import walletsPage from '../page/wallets.page'
 
 test.describe('Wallets view', () => {
@@ -27,44 +27,44 @@ test.describe('Wallets view', () => {
   })
 })
 
-// test.describe('Wallets list', () => {
-//   let wallets: ReturnType<typeof walletsPage>
+test.describe('Wallets list', () => {
+  let wallets: ReturnType<typeof walletsPage>
 
-//   test.beforeEach(async ({ page }) => {
-//     await page.goto('/')
-//     wallets = walletsPage(page)
-//   })
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+    wallets = walletsPage(page)
+  })
 
-//   test('should see empty list', async ({ page }) => {
-//     await mock(page, adminMock.ListWallets, { wallets: [] })
-//     await page.goto('/')
+  test('should see empty list', async ({ page }) => {
+    await mock(page, adminMock.ListWallets, { wallets: [] })
+    await page.goto('/')
 
-//     console.log(wallets.walletList)
-//     await expect(wallets.walletList).toBeVisible()
-//     await expect(wallets.walletList.getByRole('button')).toBeHidden()
-//   })
+    console.log(wallets.walletList)
+    await expect(wallets.walletList).toBeVisible()
+    await expect(wallets.walletList.getByRole('button')).toBeHidden()
+  })
 
-//   test('should see list with few wallets', async ({ page }) => {
-//     await mock(page, adminMock.ListWallets, {
-//       wallets: ['wallet-1', 'wallet-2', 'wallet-3'],
-//     })
-//     await page.goto('/')
+  test('should see list with few wallets', async ({ page }) => {
+    await mock(page, adminMock.ListWallets, {
+      wallets: ['wallet-1', 'wallet-2', 'wallet-3'],
+    })
+    await page.goto('/')
 
-//     await expect(wallets.walletList).toBeVisible()
-//     expect(await wallets.walletList.getByRole('button').all()).toHaveLength(3)
-//   })
+    await expect(wallets.walletList).toBeVisible()
+    expect(await wallets.walletList.getByRole('button').all()).toHaveLength(3)
+  })
 
-//   test('should see list with many wallets', async ({ page }) => {
-//     await mock(page, adminMock.ListWallets, {
-//       wallets: new Array(100).fill(0).map((_, i) => `wallet-${i}`),
-//     })
-//     await page.goto('/')
+  test('should see list with many wallets', async ({ page }) => {
+    await mock(page, adminMock.ListWallets, {
+      wallets: new Array(100).fill(0).map((_, i) => `wallet-${i}`),
+    })
+    await page.goto('/')
 
-//     await expect(wallets.walletList).toBeVisible()
-//     expect(await wallets.walletList.getByRole('button').all()).toHaveLength(100)
-//     await expect(wallets.wallet('wallet-99')).toBeVisible()
-//   })
-// })
+    await expect(wallets.walletList).toBeVisible()
+    expect(await wallets.walletList.getByRole('button').all()).toHaveLength(100)
+    await expect(wallets.wallet('wallet-99')).toBeVisible()
+  })
+})
 
 test.describe('Wallets actions', () => {
   let wallets: ReturnType<typeof walletsPage>
