dc68dd159bd97e49c3a2bcf0aa852e82713b4364
Rado <szpiechrados@gmail.com>
2023-04-24 16:31:56 +0200

    chore: fix wallets header check

--- apps/wallet-mock/src/page/wallets.page.ts
+++ apps/wallet-mock/src/page/wallets.page.ts
@@ -3,6 +3,7 @@ import type { Page } from '@playwright/test'
 const walletsPage = (page: Page) => {
   const locators = {
     home: page.getByTestId('wallet-home'),
+    header: page.getByTestId('Wallets-header'),
     walletList: page.getByTestId('wallet-list'),
     createWalletButton: page.getByTestId('create-new-wallet'),
     importWalletButton: page.getByTestId('import-wallet'),
