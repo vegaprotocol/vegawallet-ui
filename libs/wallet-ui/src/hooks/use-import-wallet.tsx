74108b8e7c1de74b213e3855baa8d971efd76c8d
Botond <botond@vegaprotocol.io>
2023-05-10 08:40:44 +0100

    fix: unlock wallet when importing / creating in the app

--- libs/wallet-ui/src/hooks/use-import-wallet.tsx
+++ libs/wallet-ui/src/hooks/use-import-wallet.tsx
@@ -56,6 +56,7 @@ export function useImportWallet() {
             type: 'ADD_WALLET',
             wallet: values.wallet,
             key: keypair,
+            auth: true,
           })
           AppToaster.show({
             message: `Wallet imported`,
