c3913e3b6dadfdded811ecae1468308a3159e936
asiaznik <artur@vegaprotocol.io>
2023-06-01 21:45:41 +0200

    feat: external links

--- libs/wallet-ui/src/routes/settings/home/home.tsx
+++ libs/wallet-ui/src/routes/settings/home/home.tsx
@@ -3,6 +3,7 @@ import { List } from '../../../components/list'
 import { Link } from 'react-router-dom'
 import { Page } from '../../../components/page'
 import { Paths } from '../..'
+import { ArrowTopRight } from '../../../components/icons/arrow-top-right'
 
 export const SettingsHome = () => {
   const items = [
@@ -18,6 +19,16 @@ export const SettingsHome = () => {
       title: 'Disclaimer',
       path: Paths.Settings.Disclaimer,
     },
+    {
+      title: 'Mainnet status and incidents',
+      path: 'https://blog.vega.xyz/tagged/vega-incident-reports',
+      external: true,
+    },
+    {
+      title: 'Share your feedback',
+      path: 'https://github.com/vegaprotocol/feedback/discussions',
+      external: true,
+    },
   ]
   return (
     <Page name="Settings">
@@ -25,17 +36,29 @@ export const SettingsHome = () => {
         items={items}
         idProp="path"
         clickable={true}
-        renderItem={({ title, path }) => (
-          <Link
-            to={{ pathname: path }}
-            className="flex justify-between text-base"
-          >
-            <div>{title}</div>
-            <div>
-              <Icon name="chevron-right" />
-            </div>
-          </Link>
-        )}
+        renderItem={({ title, path, external }) =>
+          external ? (
+            <a
+              href={path}
+              target="_blank"
+              rel="noreferrer"
+              className="flex fle-row items-center gap-2"
+            >
+              <span>{title}</span>{' '}
+              <ArrowTopRight className="w-3 h-3 stroke-white stroke-1" />
+            </a>
+          ) : (
+            <Link
+              to={{ pathname: path }}
+              className="flex justify-between text-base"
+            >
+              <div>{title}</div>
+              <div>
+                <Icon name="chevron-right" />
+              </div>
+            </Link>
+          )
+        }
       />
     </Page>
   )
